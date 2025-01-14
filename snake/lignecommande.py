4fdf873d358a54c786f7e42d7aa25c826056f374
Solène Losantos <solene.losantos@etu.minesparis.psl.eu>
2025-01-14 10:25:29 +0100

    dernières modifs

--- snake/lignecommande.py
+++ snake/lignecommande.py
@@ -19,7 +19,7 @@ def lignecommande():
     parser.add_argument('--fps', '--framepersecond', type=int, help= "number of frames per second", default=10)
     parser.add_argument('--fc', "--fruit_color", type=str, help= "color of the fruit", default= '#ff0000' )
     parser.add_argument('--hc', '--snake_head_color', type=str, help="color of the head's snake", default='#ffffff')
-    parser.add_argument('--bc', '--snake_body_color', type=str, help="color of the body's snake", default='00ff00')
+    parser.add_argument('--bc', '--snake_body_color', type=str, help="color of the body's snake", default='#00ff00')
     parser.add_argument('--tc', '--text_color', type=str, help='color of the text', default="#ff6eb4")
     parser.add_argument('--ms', '--max_scores', type= str, help = 'max of scores', default= 5 )
     args=parser.parse_args()
@@ -29,10 +29,10 @@ def lignecommande():
     if not (HEIGHT_MIN <= args.height <= HEIGHT_MAX) :
         raise IntRangeError ('height', args.height, HEIGHT_MIN, HEIGHT_MAX)
     #check argument
-    if not re.match(r"#[0-9A-Fa-f]{6}$", args.fruit_color):
-        raise ColorError(args.fruit_color, "--fruit_color")
-    if not re.match(r"#[0-9A-Fa-f]{6}$", args.snake_head_color):
-        raise ColorError(args.snake_head_color, "--snake_head_color")
-    if not re.match(r"#[0-9A-Fa-f]{6}$", args.snake_body_color):
-        raise ColorError(args.snake_body_color, "--snake_body_color")
+    if not re.match(r"#[0-9A-Fa-f]{6}$", args.fc):
+        raise ColorError(args.fc, "--fruit_color")
+    if not re.match(r"#[0-9A-Fa-f]{6}$", args.hc):
+        raise ColorError(args.hc, "--snake_head_color")
+    if not re.match(r"#[0-9A-Fa-f]{6}$", args.bc):
+        raise ColorError(args.bc, "--snake_body_color")
     return args
